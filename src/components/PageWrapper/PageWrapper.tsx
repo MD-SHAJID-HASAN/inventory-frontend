19ed1cccabce706ad71475689e0d4b7997894390
Shajid Hasan <shajid4678@gmail.com>
2025-08-25 16:55:34 +0600

    fix pagewrapper

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AddButton from "../customButton/AddButton";
 import { ChevronDown } from "lucide-react";
 
@@ -28,13 +28,18 @@ function PageWrapper({
       <div className="flex justify-between">
         <button
           onClick={handleBackClick}
-          className="flex items-center space-x-2 py-2 px-4 my-3 bg-gradient-to-r to-blue-500 from-purple-600 text-white rounded-xl hover:shadow-lg transition-all cursor-pointer"
+          className="flex items-center md:space-x-2 py-2 px-4 my-3 bg-gradient-to-r to-blue-500 from-purple-600 text-white rounded-xl hover:shadow-lg transition-all cursor-pointer"
         >
           <ChevronDown className="w-4 h-4 rotate-90"></ChevronDown>
-          <span className="text-sm font-medium">Go Back</span>
+          <span className="text-sm font-medium md:flex hidden">Go Back</span>
         </button>
-        <h2 className="dark:text-white text-2xl font-black">{pageTitle}</h2>
-        <AddButton  href={`${btnText.length <= 2 ? "/" : href}`} btnText={`${btnText.length <= 2 ? "Home" : btnText}`}></AddButton>
+        <h2 className="dark:text-white md:text-2xl font-black text-center">
+          {pageTitle}
+        </h2>
+        <AddButton
+          href={`${btnText.length <= 2 ? "/" : href}`}
+          btnText={`${btnText.length <= 2 ? "Home" : btnText}`}
+        ></AddButton>
       </div>
 
       {children}
--- src/components/customButton/AddButton.tsx
+++ src/components/customButton/AddButton.tsx
@@ -11,10 +11,10 @@ const AddButton: React.FC<IProps> = ({ btnText, href }) => {
   return (
     <Link
       to={`${href}`}
-      className="flex items-center space-x-2 py-2 px-4 my-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-xl hover:shadow-lg transition-a cursor-pointer"
+      className="flex items-center md:space-x-2 py-2 px-4 my-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-xl hover:shadow-lg transition-a cursor-pointer"
     >
       <Plus className="w-4 h-4"></Plus>
-      <span className="text-sm font-medium">{btnText}</span>
+      <span className="text-sm font-medium md:flex hidden">{btnText}</span>
     </Link>
   );
 };
