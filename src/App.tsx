9f6d8f26a5e13c7e233b57d41fefdc289cb71e7d
Shajid Hasan <shajid4678@gmail.com>
2025-08-27 18:34:16 +0600

    shop card update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import AppRoutes from "./routes/AppRoutes";
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-blue-200 via-blue-00 to-purple-300 dark:from-slate-800 dark:to-slate-900 transition-all duration-500 overflow-hidden">
         <AppRoutes></AppRoutes>
-        <Analytics></Analytics>
       </div>
     </>
   );
--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -9,9 +9,10 @@ const ShopCard: React.FC<ShopCardProps> = ({ shop }) => {
   console.log(shop)
   return (
     <Link to={`/shops/${shop._id}`}>
-      <div className="border border-slate-600 rounded-xl p-4 shadow-md dark:bg-slate-900/80 dark:text-white max-w-sm hover:bg-slate-800/80 min-w-72">
-        <img className="w-72 h-40 rounded-xl mb-2" src={`/images/shop.jpg`} alt="Shop Image" />
-        <h2 className="text-xl font-bold mb-2">{shop.name}</h2>
+      <div className="border border-slate-600 rounded-xl shadow-md dark:bg-slate-900/80 dark:text-white max-w-sm hover:bg-slate-800/80 min-w-72 mt-4">
+        <img className="w-full h-40 rounded-t-xl mb-2" src={`/images/shop.jpg`} alt="Shop Image" />
+        <div className="px-2 pb-2">
+          <h2 className="text-xl font-bold mb-2">{shop.name}</h2>
         <p className="hidden">
           <strong>Owner:</strong> {shop.owner}
         </p>
@@ -26,6 +27,7 @@ const ShopCard: React.FC<ShopCardProps> = ({ shop }) => {
           <span>Categories: {shop.totalCategories}</span>
           <span>Products: {shop.totalProducts}</span>
           <span>Stock: {shop.totalStock}</span>
+        </div>
         </div>
       </div>
     </Link>
